7b8efe86af8bd008ae7297210b5d61313011e0fa
Pedro Marcos Fernandes Costa <120522530+Pedrox0944@users.noreply.github.com>
2025-02-11 17:08:59 -0300

    Isso é uma lista encadeada

--- listaencadeada.c
+++ listaencadeada.c
@@ -1,137 +1,141 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Definicao do nó da lista encadeada
-
-typedef struct No{
+typedef struct Node 
+{
     int value;
-    struct No* prox;
-}No;
+    struct Node *prox;
+} node;
 
-//função para adicionar um novo no na lista
-void insertno(No** head, int value){
-    No* newno = (No*)malloc(sizeof(No)); // Aloca memória para o novo nó
-    newno->value = value; // Atribui o valor ao novo nó
-    newno->prox = NULL; // Faz o 'prox' apontar para NULL
+typedef struct 
+{
+    node *start;
+    node *end;
+    node *assistant;
+    int size_list;
+} simple_list;
 
-    if (*head == NULL) {
-        // Se a lista estiver vazia, o novo nó será o primeiro
-        *head = newno;
+simple_list *create_list()
+{
+    simple_list *new = malloc(sizeof(simple_list));
+    new->start = NULL;
+    new->end = NULL;
+    new->assistant = NULL;
+    new->size_list = 0;
+    return new;
+}
+
+node *create_node(int value)
+{
+    node *new = malloc(sizeof(node));
+    new->value = value;
+    new->prox = NULL;
+    return new;
+}
+
+void add(simple_list *list, node *node, int pos)
+{
+    if (list == NULL || node == NULL) return;
+  
+    if (list->start == NULL) 
+    {
+        list->start = node;
+        list->end = node;
+    }
+    else if (pos == 0) 
+    {
+        node->prox = list->start;
+        list->start = node;
+    }
+    else if (pos == list->size_list) 
+    {
+        list->end->prox = node;
+        list->end = node;
+    }
+    else 
+    {
+        list->assistant = list->start;
+        for (int i = 0; i < pos-1 ; i++) 
+        {
+            list->assistant = list->assistant->prox;
+        }
+        node->prox = list->assistant->prox;
+        list->assistant = node;
+    }
+    list->size_list++;
+}
+
+int delete(simple_list *list, int pos)
+{
+    if (list->size_list >= 0 && pos < list->size_list) 
+    {
+        node *trash = list->start;
+        if (list->size_list == 1) 
+        {
+            trash = list->start;
+            list->start = NULL;
+            list->end = NULL;
+        }
+        else if (pos == 0) 
+        {
+            trash = list->start;
+            list->start = list->start->prox;
+        }
+        else if(pos == list->size_list - 1)
+        {
+            list->assistant = list->start;
+            for (int i = 0; i < pos-1; i++) 
+            {
+                trash = list->end;
+                list->end = list->assistant;
+                list->end->prox = NULL;
+            }
+        }
+        else 
+        {
+            list->assistant = list->start;
+            for (int i = 0; i < pos-1; i++) 
+            {
+                list->assistant = list->assistant->prox;
+            }
+            trash = list->assistant->prox;
+            list->assistant->prox = trash->prox;
+        }
+        free(trash);
+        list->size_list--;
+        return trash->value;
     }
     else {
-        // Se a lista não estiver vazia, percorre até o último nó
-        No* temp = *head;
-        while (temp->prox != NULL) {
-            temp = temp->prox;
-        }
-        // Depois de achar o último nó, faz o 'prox' apontar para o novo nó
-        temp->prox = newno;
+        puts("List empty");
+        return -1;
     }
 }
-//função para imprimir a lista 
-void readlist(No* head){
-    if(head == NULL){
-        printf("A lista esta vazia!\n");
-        return;
+
+void print(simple_list *list)
+{
+    puts("--- START LIST ---");
+    list->assistant = list->start;
+    while (list->assistant != NULL) 
+    {
+        printf("%d\n", list->assistant->value);
+        list->assistant = list->assistant->prox;
     }
-    No* temp = head;
-    printf("lista de nos: \n");
-    while(temp != NULL){
-        printf("%d -> ",temp->value); //exibe o valor do nó
-        temp = temp->prox; //avança para o proximo nó
-    }
-    printf("NULL\n");
-}   
-//função update atuliazar nó
-void updateno(No* head, int oldvalue, int newvalue){
-    No* temp = head;
-    while(temp != NULL){
-        if(temp->value==oldvalue){
-            temp->value=newvalue; //atualiza o valor do nó  
-            printf("o valor %d foi atualizado para %d ! \n",oldvalue,newvalue);
-            return;
-        }
-        temp = temp->prox;
-    }
-    printf("valor %d nao foi encontrado na lista!\n",oldvalue);
-}
-//função delete 
-void deleteno(No** head, int value){
-    if(*head == NULL){
-        printf("a lista esta vazia!\n");
-        return;
-    }
-    No* temp = *head;
-    No* prev = NULL;
-    //Se o nó a ser removido é o primeiro nó
-    if(temp != NULL && temp->value==value){
-        *head = temp->prox; //atualiza o head para o prox nó
-        free(temp);//libera a memoria do no removido
-        printf("o valor %d foi removido !\n",value);
-        return;
-    }
-    //caso o nó a ser removido não seja o primeiro
-    while(temp != NULL && temp->value != value){
-        prev = temp;
-        temp = temp->prox;
-    }
-    //se o valor não for encontrado
-    if(temp == NULL){
-        printf("o valor %d nao foi encontrado !\n", value);
-        return;
-    }
-    prev->prox = temp->prox; //desconecta o nó a ser removido
-    free(temp); //libera a memoria do no removido
-    printf("o valor %d foi removido !\n",value);
+    puts("--- END LIST ---");
+    puts("");
 }
 
-//criação da main 
+int main()
+{
+    simple_list *list = create_list();
+    node *firtsnode = create_node(10);
+    node *secodnode = create_node(9);
+    node *finalnode = create_node(8);
+    add(list, firtsnode, 0);
+    add(list, secodnode, 1);
+    add(list, finalnode, 2);
+    print(list);
+    delete(list, 0);
+    print(list);
 
-int main(){
-    No* head = NULL; //inicializa a lista vazia
-    int choice, value, newvalue, oldvalue;
-
-    while(1){
-         printf("\nEscolha uma operacao:\n");
-        printf("1. Adicionar (Criar) um valor\n");
-        printf("2. Exibir a lista\n");
-        printf("3. Atualizar um valor\n");
-        printf("4. Remover um valor\n");
-        printf("5. Sair\n");
-        printf("Opcao: ");
-        scanf("%d", &choice);
-
-        if(choice == 1){
-            printf("Digite o valor a ser adicionado: \n");
-            scanf("%d",&value);
-            insertno(&head,value);
-        } else if(choice == 2){
-            readlist(head);
-        } else if(choice == 3){
-            printf("Digite o valor que deseja atualizar: \n");
-            scanf("%d",&oldvalue);
-            printf("Digite o novo valor: \n");
-            scanf("%d",&newvalue);
-            updateno(head,oldvalue,newvalue);
-        } else if(choice == 4){
-            printf("Digite o valor que deseja remover: \n");
-            scanf("%d",&value);
-            deleteno(&head,value);
-        } else if(choice == 5){
-            break;
-        } else{
-            printf("Opcao invalida! \n");
-        }
-    }
-        //libera a memoria da lista ao final
-        No* temp = head;
-        while(temp != NULL){
-            No* next = temp->prox;
-            free(temp);
-            temp = next;
-        }
-    
     return 0;
 }
-
-
